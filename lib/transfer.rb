959f3bfc132c3aa08252b2fc46a1073700fd4dd1
Sam Bennrubi <sbennrubi@gmail.com>
2019-01-12 21:18:58 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -23,7 +23,7 @@ class Transfer
   end
   
   def reverse_transfer
-    if valid? && receiver.balance > amount && self.status == "pending"
+    if valid? && receiver.balance > amount && self.status == "complete"
       receiver.balance -= amount
       sender.deposit(amount)
       self.status = "reversed"
