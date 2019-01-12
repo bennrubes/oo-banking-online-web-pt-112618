28b93a9655ceaf0beea61aa47c63728ebb15d5f0
Sam Bennrubi <sbennrubi@gmail.com>
2019-01-12 21:10:55 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,12 +1,20 @@
 class Transfer
-  attr_reader :sender, :receiver
-  def initialize(sender, receiver)
+  attr_reader :sender, :receiver, :amount
+  attr_accessor :status
+  def initialize(sender, receiver, amount)
     @sender = sender
     @receiver = receiver
-  
+    @status = "pending"
+    @amount = amount
   end
   
+  def valid?
+  end
   
+  def execute_transaction
+  end
   
+  def reverse_transaction
+end
   
 end
